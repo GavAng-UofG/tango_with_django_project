645fe84f155a63a7b1c0aa02fa96a3212d809ddb
Gavin Angus <gavin.angus04@gmail.com>
2024-02-08 14:46:13 +0000

    partially completed chapter 9

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 # Register your models here.
 
@@ -16,3 +16,6 @@ class PageAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Page, PageAdmin)
+
+
+admin.site.register(UserProfile)
